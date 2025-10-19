31843e2cf01c24e3e7e13c50aaab83f55d675248
Henrique Jorge Barateli <henriquejb194@gmail.com>
2025-10-19 07:06:09 -0300

    refactor(#34): remove privileged net clients

--- src/common/include/quakedef.h
+++ src/common/include/quakedef.h
@@ -163,10 +163,6 @@
 
 #define SOUND_CHANNELS 8
 
-// This makes anyone on id's net privileged
-// Use for multiplayer testing only - VERY dangerous!!!
-// #define IDGODS
-
 #include "common.h"
 
 #endif
